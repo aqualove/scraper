fc6b078644fc682c31c300a91f1a3101532773ec
Hadrien David <hadrien@ectobal.com>
2016-06-17 16:30:05 -0400

    Add test setup + test requirements.

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().split()
 
+with open('requirements-test.txt') as f:
+    test_requirements = f.read().split()
+
 with open('VERSION') as f:
     version = f.read().strip()
 
@@ -11,5 +14,7 @@ setup(
     name='Scraper',
     version=version,
     packages=find_packages(),
-    setup_requires=requirements,
+    install_requires=requirements,
+    tests_require=test_requirements,
+    test_suite='scraper.tests',
 )
